2822b76da29d19cd2d2ebf69f0bb616471a6df9b
Marlon Prado <marlonprado04@gmail.com>
2023-10-11 02:17:50 -0300

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MediumCardComponent } from './components/medium-card/medium-card.component';
+import { TextCardComponent } from './components/text-card/text-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuCardComponent } from './components/menu-card/menu-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BannerComponent,
+    MediumCardComponent,
+    TextCardComponent,
+    SmallCardComponent,
+    BigCardComponent,
+    MenuCardComponent
   ],
   imports: [
     BrowserModule,
