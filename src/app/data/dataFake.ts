322473dc1f2c941ff5573419e4ba286ab7cc5bbf
Marlon Prado <marlonprado04@gmail.com>
2023-10-16 19:46:07 -0300

    feat: add dynamism to content page

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
 
   {
-    "id": 1,
-    "photo": "aaa",
+    "id": "1",
+    "photo": "https://proxy.olhardigital.com.br/wp-content/uploads/2022/07/one-piece-1024x576.jpg",
     "title": "saiu nova série",
-    "description": "olá"
+    "description": "olá galerinha"
   },
 
   {
-    "id": 2,
+    "id": "2",
     "photo": "bbb",
     "title": "saiu nova temporada",
     "description": "hello"
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -8,9 +9,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContentComponent implements OnInit {
 
-  photoCover: string = "https://proxy.olhardigital.com.br/wp-content/uploads/2022/07/one-piece-1024x576.jpg";
-  contentTitle: string = "Notítica de exemplo";
-  contentDescription: string = "Descrição de exemplo";
+  contentTitle: string = "";
+  contentDescription: string = "";
+  contentImage: string = "";
+
+  // Criando id para a content page (necessário aceitar null por exigência da route.paramMap.subscribe abaixo)
+  private contentId: string | null = "0";
 
   // Adiciona parâmetro para gerenciar rotas no método construtor do componente
   constructor(private route: ActivatedRoute) {
@@ -19,8 +23,26 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
 
-    // Ao iniciar componente, pega a rota que redirecionou para esta página e imprime o id passado da rota no console
-    this.route.paramMap.subscribe( value => console.log(value.get("id")))
+    // Passando a id passada ao clicar no card para a id da content page
+    this.route.paramMap.subscribe(value => {
+      this.contentId = value.get("id")
+    })
+
+    // Chamando método para puxar informações do dataFake a partir do id carregado do componente anterior
+    this.setValuesToComponent(this.contentId);
+  }
+
+  // Cria função para setar informações do componente de forma dinâmica a partir do dataFake importado
+  setValuesToComponent(id: string | null) {
+
+    // Filtra o artigo (dataFake) de acordo com o id que foi armazenado nesta página de conteúdo (content page) e foi passado pelo componente anterior
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    // Atribuindo às variáveis dessa páginas as informações extraídas do dataFake
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.contentImage = result.photo;
+
   }
 
 }
